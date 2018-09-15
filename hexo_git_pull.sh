fa9ce9c7ffcb76a6648db059e679630f551391c9
Kevin Zeng <503132987@qq.com>
2018-09-15 22:33:01 +0800

    修改颜色样式

---
+++ hexo_git_pull.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd /Users/Kevin/Git/huajingzeng.github.io
+echo $NODE_PATH
+export NODE_PATH=/usr/local/lib/node_modules
+git pull origin hexo
--- hexo_git_push.sh
+++ hexo_git_push.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd /Users/Kevin/Git/huajingzeng.github.io
+echo $NODE_PATH
+export NODE_PATH=/usr/local/lib/node_modules
 git pull origin hexo
 git add .
 git commit -m "Update posts"
